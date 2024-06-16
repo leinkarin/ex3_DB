ae914e8afdb0a033e8e024fe1a0d9afad7885c1d
karin-lein <karine.lein@mail.huji.ac.il>
2024-06-16 17:30:29 +0300

    q4

--- q4.sql
+++ q4.sql
@@ -1,8 +1,6 @@
 SELECT name, year
-FROM authors a1
+FROM authors a1 NATURAL JOIN conferences c1
 WHERE institution = 'Hebrew University of Jerusalem'
-  AND conference IN (SELECT conference FROM conferences WHERE subarea = 'ai')
-  AND EXISTS (SELECT 1 FROM authors a2 WHERE a1.name = a2.name AND a1.year = a2.year AND a2.conference IN (SELECT conference FROM conferences WHERE subarea = 'economics'))
+  AND c1.subarea = 'ai'
+  AND EXISTS (SELECT name FROM authors a2 NATURAL JOIN conferences c2 WHERE a1.name = a2.name AND a1.year = a2.year AND c2.subarea = 'economics')
 ORDER BY name, year;
-
-
